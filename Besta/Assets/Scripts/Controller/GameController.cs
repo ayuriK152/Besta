e18036db65b4ca4b3b32425ad16bc30729ea447b
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-07-11 20:44:50 +0900

    feat: 인게임용 노트 프리팹 및 스크립트 생성

--- Besta/Assets/Scripts/Controller/GameController.cs
+++ Besta/Assets/Scripts/Controller/GameController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Datas;
+using static Define;
 
 public class GameController : MonoBehaviour
 {
+    public Queue<GameObject> firstLaneNotes;
+    public Queue<GameObject> secondLaneNotes;
+    public Queue<GameObject> thirdLaneNotes;
+    public Queue<GameObject> fourthLaneNotes;
     void Start()
     {
         Managers.Input.KeyAction -= PlayerKeyDown;
@@ -18,5 +24,28 @@ public class GameController : MonoBehaviour
     public void PlayerKeyDown(KeyCode key)
     {
         Debug.Log(key.ToString());
+    }
+
+    public void LoadPattern()
+    {
+        firstLaneNotes = new Queue<GameObject>();
+        secondLaneNotes = new Queue<GameObject>();
+        thirdLaneNotes = new Queue<GameObject>();
+        fourthLaneNotes = new Queue<GameObject>();
+
+        foreach (Note n in Managers.Game.currentLoadedPattern._noteDatas)
+        {
+            switch (n._laneNumber)  // 레인별로 입력을 처리하기 위해 따로 큐에 저장한 후 선입선출 방식으로 처리
+            {
+                case LaneNumber.First:
+                    break;
+                case LaneNumber.Second:
+                    break;
+                case LaneNumber.Third:
+                    break;
+                case LaneNumber.Fourth:
+                    break;
+            }
+        }
     }
 }
---
+++ Besta/Assets/Scripts/GameNote.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using static Datas;
+
+public class GameNote : MonoBehaviour
+{
+    public Note data;
+    GameObject _startPoint;
+    GameObject _longNotePole;
+    public GameObject longNotePole { get { return _longNotePole; } }
+    GameObject _endPoint;
+    public GameObject endPoint { get { return _endPoint; } }
+
+    void Awake()
+    {
+        _startPoint = transform.Find("Start").gameObject;
+        _longNotePole = transform.Find("LongNotePole").gameObject;
+        _endPoint = transform.Find("End").gameObject;
+    }
+
+    public void ResizePole()
+    {
+        float positionDifference = _endPoint.transform.localPosition.y - _startPoint.transform.localPosition.y;
+        _longNotePole.transform.localPosition = new Vector3(_startPoint.transform.localPosition.x, positionDifference / 2 + _startPoint.transform.localPosition.y, 1);
+        _longNotePole.transform.localScale = new Vector3(1, positionDifference / 0.25f, 1);
+    }
+}
